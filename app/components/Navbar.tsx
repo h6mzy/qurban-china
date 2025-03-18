3767d1cdc09033ddddf789be7e90b38d3f3d9891
h6mzy <112537750+h6mzy@users.noreply.github.com>
2025-03-18 08:41:36 +0000

    combined nav wip

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import Link from 'next/link'
-import MobileNav from './MobileNav'
 import styles from './Navbar.module.sass'
+import { AnimatePresence } from 'motion/react'
+import { useState } from 'react'
+import * as motion from 'motion/react-client'
 
 const leftNav = ["watch", "contact"]
 const rightNav = ["zakat", "projects", "testimonials"]
@@ -12,6 +14,7 @@ const Logo = () => (
 )
 
 const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(false)
   return (
     <header className={styles.navbar}>
       <nav className={styles.nav} id='left-nav'>
@@ -38,7 +41,44 @@ const Navbar = () => {
           )}
         </ul>
       </nav>
-      <MobileNav logo={<Logo />} items={leftNav.concat(rightNav)} />
+      <div className={styles.container}>
+        <motion.button
+          className={styles.menuButton}
+          onClick={() => setIsVisible(!isVisible)}
+          whileTap={{ y: 1 }}
+        >
+          {isVisible ? 'X' : 'O'}
+        </motion.button>
+        <AnimatePresence initial={false}>
+          {isVisible ? (
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+              transition={{ duration: .2 }}
+              className={`flex center center ${styles.mask}`}
+              key="box"
+              onClick={() => setIsVisible(false)}
+            >
+              <div className={`flex column nowrap alignCenter fullWidth fullHeight ${styles.box}`}>
+                <Logo />
+                <nav className={styles.nav}>
+                  <ul className={styles.leftList}>
+                    <li className={styles.item}>
+                      <Link className={styles.donateButton} href={`/donate`}>donate</Link>
+                    </li>
+                    {leftNav.concat(rightNav).map((item, key) =>
+                      <li className={styles.item} key={key}>
+                        <Link className={styles.button} href={`/${item}`}>{item}</Link>
+                      </li>
+                    )}
+                  </ul>
+                </nav>
+              </div>
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
+      </div>
     </header>
   )
 }
