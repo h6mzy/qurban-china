b6d4165ff41e2fd47e98c9bc833139df6339f0b4
h6mzy <112537750+h6mzy@users.noreply.github.com>
2025-03-27 08:17:20 +0000

    move examples to readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## To-do List
 
 - Add prominent current project cta
+
+//https://codesandbox.io/p/sandbox/framer-motion-sharedlayout-dk5pfh?file=%2Fsrc%2FApp.js%3A140%2C28
+//https://codesandbox.io/p/sandbox/framer-motion-ios-notifications-0sct9o?file=%2Fsrc%2FApp.js
+//https://framermotionexamples.com/example/app-store-ui-using-react-and-framer-motion
+//https://codesandbox.io/p/sandbox/app-store-ui-using-react-and-framer-motion-ecgc2?file=%2Fsrc%2Findex.tsx
--- app/page.tsx
+++ app/page.tsx
@@ -57,9 +57,3 @@ export default function Home() {
     </main>
   )
 }
-
-//https://codesandbox.io/p/sandbox/framer-motion-sharedlayout-dk5pfh?file=%2Fsrc%2FApp.js%3A140%2C28
-//https://codesandbox.io/p/sandbox/framer-motion-ios-notifications-0sct9o?file=%2Fsrc%2FApp.js
-//https://framermotionexamples.com/example/app-store-ui-using-react-and-framer-motion
-//https://codesandbox.io/p/sandbox/app-store-ui-using-react-and-framer-motion-ecgc2?file=%2Fsrc%2Findex.tsx
-//
