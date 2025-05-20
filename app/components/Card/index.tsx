202cffb58846cd0e7b91d816413a51ae082988fd
h6mzy <112537750+h6mzy@users.noreply.github.com>
2025-05-20 07:18:53 +0000

    wip

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -7,10 +7,22 @@ export default function Card({ project = {}, index = 1 }:{ project:any, index?:n
  return (
     <motion.div
       layoutId={project.id}
-      className={styles.card}
-      style={{ background: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, .5)), url(https://i.pravatar.cc/768?img=${index+1}) center/cover no-repeat` }}
+      layout
+      transition={{ type: 'tween', duration: .3, ease: 'easeInOut' }}
+      className={styles.cardWrapper}
     >
-      <h3>{project.title}</h3>
+      <div className={styles.card}>
+        <motion.div 
+          layoutId={`${project.id}-cover`}
+          className={styles.cover}
+        >
+          <img
+            src={`https://i.pravatar.cc/768?img=${index+1}`}
+            alt={project.title}
+          />
+        </motion.div>
+        <h3>{project.title}</h3>
+      </div>
     </motion.div>
   )
 }
--- app/components/ExpandedCard/index.tsx
+++ app/components/ExpandedCard/index.tsx
@@ -1,50 +1,26 @@
 'use client'
 
 import { motion } from 'motion/react'
-import { useEffect } from 'react'
-import styles from '@/app/components/Card/Card.module.sass'
+import styles from '@/app/components/ExpandedCard/ExpandedCard.module.sass'
 import button from '@/styles/Button.module.sass'
-import layout from '@/styles/Layout.module.sass'
-import Image from 'next/image'
 
 export default function ExpandedCard({ project, onClose, index = 1 }: { project: any; onClose: () => void, index?:number }) {
-  // Disable scroll on body and html when the expanded card is open
-  useEffect(() => {
-    // Lock scrolling for the whole page (html and body)
-    document.documentElement.style.overflow = 'hidden';  // html
-    document.body.style.overflow = 'hidden'; // body
-
-    // Cleanup function to restore scroll when modal closes
-    return () => {
-      document.documentElement.style.overflow = ''; // Unlock html
-      document.body.style.overflow = ''; // Unlock body
-    };
-  }, []);
   return (
     <motion.div
-      className={styles.expandedCardWrapper}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: .3, ease: 'easeInOut' }}
+      layoutId={project.id}
+      layout
+      transition={{ type: 'tween', duration: .3, ease: 'easeInOut' }}
+      className={styles.cardWrapper}
       onClick={onClose}
     >
       <motion.div
-        layoutId={project.id}
-        className={styles.expandedCard}
-        initial={{ scale: .9 }}
-        animate={{ scale: 1 }}
-        exit={{ scale: .9, opacity: 0 }}
-        transition={{ duration: .3, ease: 'easeOut' }}
+        className={styles.card}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className={styles.image}>
-          <Image
-            src={`https://i.pravatar.cc/768?img=${index+1}`}
-            alt={project.title}
-            fill
-            className={layout.cover}
-          />
+        <motion.div 
+          layoutId={`${project.id}-cover`}
+          className={styles.cover}
+        >
           <motion.button
             aria-label="Close modal"
             className={`${button.circle} ${button.close}`}
@@ -53,7 +29,11 @@ export default function ExpandedCard({ project, onClose, index = 1 }: { project:
           >
             x
           </motion.button>
-        </div>
+          <img
+            src={`https://i.pravatar.cc/768?img=${index+1}`}
+            alt={project.title}
+          />
+        </motion.div>
         <div className={styles.body}>
           <h2>{project.title}</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
